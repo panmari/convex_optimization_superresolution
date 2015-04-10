ecf49d5dcb6c7302f942213b12239eb0ea3d8daf
panmari <smmuzi@gmail.com>
2015-04-10 12:17:37 +0200

    Fixed fitting term to make much more sense.

--- superresolution/superresolution_sm.m
+++ superresolution/superresolution_sm.m
@@ -12,6 +12,7 @@ SRfactor = sqrt(MND/MN);
 M = MD / SRfactor;
 N = ND / SRfactor;
 u = D'*g(:); % initial guess for solution u
+DTg = D' * g(:); % Precomputed here instead of in every iteration.
 t = 0.01;
 max_iterations = 3000;
 gradient_norms = zeros(max_iterations,1);
@@ -24,22 +25,8 @@ for i=1:max_iterations
     grad_reg_term_y = 2*(2*u(:,:) - [u(2:end,:); u(end - 1,:)] - [u(2,:); u(1:end-1,:)]);
     grad_reg_term = grad_reg_term_x + grad_reg_term_y;
     
-    grad_other_term = D*u(:) - g(:);
-    % multiply additionally with D for inner gradient (chain rule!)
-    grad_other_term_ext2 = zeros(M,N);
-    %grad_other_term = diag(D).*grad_other_term;
-    
-    % constrain all 3 created pixels the same way as original:
-    grad_other_term_ext = repmat(grad_other_term, [1,2])';
-    % alternatively don't constrain them at all:
-    % grad_other_term_ext = [grad_other_term zeros(length(grad_other_term), 1)]';
-    grad_other_term_ext = reshape(grad_other_term_ext(:), M, N/2);
-   
-    for row=1:N
-        %if (mod(row, 2) == 0)
-            grad_other_term_ext2(:,row) = grad_other_term_ext(:,ceil(row/2));
-        %end
-    end
+    % Derivative of 1/2 || Du - g ||^2
+    grad_fitting_term = D' * D * u(:) - DTg;
     
     % add up to get gradient of whole energy term
     if (DEBUG)
@@ -49,7 +36,7 @@ for i=1:max_iterations
         imtool(3*debug_img);
     end
 
-    gradient_e = grad_reg_term(:) + lambda*grad_other_term_ext2(:);
+    gradient_e = grad_reg_term(:) + lambda*grad_fitting_term(:);
     % Update u
     u = u(:) - gradient_e*t;
     
