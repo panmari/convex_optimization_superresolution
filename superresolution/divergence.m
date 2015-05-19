699ae239c4d974644e09f8b2659dafd7964e85f6
panmari <smmuzi@gmail.com>
2015-05-19 10:42:10 +0200

    Implemented divergence, can compute forward and backward.

--- superresolution/divergence.m
+++ superresolution/divergence.m
@@ -1,14 +1,18 @@
-function [ div ] = divergence( u )
-    % TODO: Adapt amount of padding, prolly 1 is enough.
-    
+function [ div ] = divergence(u, forward)    
     % The inital u can be accessed via u_pad(2:end-1, 2:end-1).
-    u_pad = padarray(u, [1 1], 'symmetric', 'both'); 
-    % Gradients (with one padding into each direction)
-    u_x_grad = u_pad(2:end,2:end-1) - u_pad(1:end-2,2:end-1);
-    u_y_grad = u_pad(2:end-1,2:end) - u_pad(2:end-1,1:end-2);
+    if forward
+        % forward gradients, n_i - n_{i - 1}
+        u_pad = padarray(u, [1 1], 'symmetric', 'pre'); 
+        u_x_grad = u_pad(2:end,2:end) - u_pad(1:end-1, 2:end);
+        u_y_grad = u_pad(2:end,2:end) - u_pad(2:end, 1:end-1);
+    else
+        % backward gradients, n_i - n_{i + 1}
+        u_pad = padarray(u, [1 1], 'symmetric', 'post');
+        u_x_grad = u_pad(1:end-1,1:end-1) - u_pad(2:end, 1:end-1);
+        u_y_grad = u_pad(1:end-1,1:end-1) - u_pad(1:end-1, 2:end);
+    end
 
-    % Combine x and y gradient into one big matrix.
-    div = u_x_grad;
-    div(:,:,2) = u_y_grad;
+    % Combine x and y gradient to divergence.
+    div = u_x_grad + u_y_grad;
 end
 
