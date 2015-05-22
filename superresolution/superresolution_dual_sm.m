88b5efd5414e9960210d0a4ce69e79fb1c67eb8a
panmari <smmuzi@gmail.com>
2015-05-22 16:56:23 +0200

    Fixed computation of energy, x_n1 and y_n1_denominator.

--- superresolution/superresolution_dual_sm.m
+++ superresolution/superresolution_dual_sm.m
@@ -15,13 +15,13 @@ N = ND / SRfactor;
 
 % Constants
 % K_a is upper bound.
-K_a = sqrt(4);
+K_a = 8;
 % Should be rather small
-tau = 10*0.2*1e-5; 
+tau = 1e-3; 
 % Should be rather large
 sigma = 1/(tau*K_a);
 
-theta = 0.1;
+theta = 1;
 
 % Initialize all variables.
 y_n = zeros(M, N, 2);
@@ -35,20 +35,20 @@ x_n1_matrix = speye(size(DTD)) + tau * lambda * DTD;
 %x_n1_divisor = inv(x_n1_matrix);
 x_n1_right_summand = reshape(tau * lambda * D' * g(:), M, N);
 % Maximum number of iterations learnt
-max_iterations = 1000;
+max_iterations = 2000;
 % For displaying changes in costs later on.
 costs = zeros(max_iterations,1);
 
 for i=1:max_iterations
     % Compute y_n1
     y_n1_nominator = y_n + sigma * gradient(xbar_n, true);
-    y_n1_denominator = max(1, l2_norm(y_n1_nominator));
+    y_n1_denominator = max(1,  sqrt(sum(y_n1_nominator.^2, 3)));
     y_n1(:,:,1) = y_n1_nominator(:,:,1) ./ y_n1_denominator;
     y_n1(:,:,2) = y_n1_nominator(:,:,2) ./ y_n1_denominator;
 
     % Compute x_n1
     div_y_n1 = divergence(y_n1, false);
-    x_n1 = x_n1_matrix \ reshape(x_n + tau * div_y_n1 + x_n1_right_summand, [], 1);
+    x_n1 = x_n1_matrix \ reshape(x_n - tau * div_y_n1 + x_n1_right_summand, [], 1);
     x_n1 = reshape(x_n1, M, N);
     
     % Compute xbar_n1
@@ -57,8 +57,12 @@ for i=1:max_iterations
     % Adapt for next timestep, n + 1 -> n
     y_n = y_n1;
     x_n = x_n1;
+    if mod(i, 50) == 1
+       disp('Update!');
+       imagesc([xbar_n, imresize(g,[M N],'nearest')]);
+       drawnow
+    end
     xbar_n = xbar_n1;
-    
     costs(i) = energy_term_for(xbar_n, g, D, lambda);
 end
 u = reshape(xbar_n,M,N);
@@ -67,7 +71,9 @@ end
 function E_u = energy_term_for(u, g, D, lambda)
     similarity = D * u(:) - g(:);
     smoothness = gradient(u, true);
-    E_u = (lambda*0.5)*l2_norm(similarity) + l2_norm(smoothness);
+    smoothness_sqr = smoothness.^2;
+    smoothness_energy = sum(sqrt(smoothness_sqr(:)));
+    E_u = (lambda*0.5)*l2_norm(similarity) + smoothness_energy;
 end
 
 % Computes the l2 norm of a matrix/vector, i. e. squares every element,
