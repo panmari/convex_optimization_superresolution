2eb990ce48e941888ffef23480713faaaaaa1063
panmari <smmuzi@gmail.com>
2015-05-22 16:55:23 +0200

    Simplified computation of gradient

--- superresolution/gradient.m
+++ superresolution/gradient.m
@@ -2,13 +2,13 @@ function [ grad ] = gradient(u, forward)
     if forward
         % forward gradients, n_i - n_{i - 1}
         u_pad = padarray(u, [1 1], 'symmetric', 'pre'); 
-        u_x_grad = u_pad(2:end,2:end) - u_pad(1:end-1, 2:end);
-        u_y_grad = u_pad(2:end,2:end) - u_pad(2:end, 1:end-1);
+        u_x_grad = u - u_pad(1:end-1, 2:end);
+        u_y_grad = u - u_pad(2:end, 1:end-1);
     else
         % backward gradients, n_i - n_{i + 1}
         u_pad = padarray(u, [1 1], 'symmetric', 'post');
-        u_x_grad = u_pad(1:end-1,1:end-1) - u_pad(2:end, 1:end-1);
-        u_y_grad = u_pad(1:end-1,1:end-1) - u_pad(1:end-1, 2:end);
+        u_x_grad = u - u_pad(2:end, 1:end-1);
+        u_y_grad = u - u_pad(1:end-1, 2:end);
     end
     
     grad = u_x_grad;
